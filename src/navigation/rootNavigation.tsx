3cd29d74013af012dc6c4dcd296d1643432a8baa
ngogiatuan <tuanngps27012@fpt.edu.vn>
2025-06-03 16:04:55 +0700

    refactor: Loại bỏ màn hình login và register khỏi RootNavigation

--- src/navigation/rootNavigation.tsx
+++ src/navigation/rootNavigation.tsx
@@ -3,18 +3,13 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { nav } from './navigationName';
 import BottomTab from './bottomNavigation';
 import HelperScreen from '../screens/HelperScreen';
-import LoginScreen from '../screens/LoginScreen';
-import RegisterScreen from '../screens/RegisterScreen';
-import OnboardingScreen from '../screens/OnboardingScreen';
+
 
 const Stack = createNativeStackNavigator();
 const RootNavigation = () => {
     return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Onboarding" screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Onboarding" component={OnboardingScreen} options={{headerShown: false}} />
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Register" component={RegisterScreen} />
         <Stack.Screen name={nav.home} component={BottomTab} />
         <Stack.Screen name="Helper" component={HelperScreen} options={{title: 'Hướng dẫn nấu ăn'}} />
         {/* <Stack.Screen name="Details" component={DetailsScreen} /> */}
