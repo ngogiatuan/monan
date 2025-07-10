579c6637c571fc5d024b860f8bdd86c69a09344a
Ngô Gia Tuấn <tuanngps27012@fpt.edu.vn>
2025-07-10 17:39:22 +0700

    đang setup zalopay

--- android/app/src/main/java/com/monngonmoingay/MainActivity.kt
+++ android/app/src/main/java/com/monngonmoingay/MainActivity.kt
@@ -5,6 +5,8 @@ import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 import android.os.Bundle
+import com.zalopay.sdk.ZaloPaySDK
+import com.zalopay.sdk.Environment
 
 class MainActivity : ReactActivity() {
 
@@ -23,5 +25,6 @@ class MainActivity : ReactActivity() {
       
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(null)
+    ZaloPaySDK.init(<appID>, Environment);
   }
 }
